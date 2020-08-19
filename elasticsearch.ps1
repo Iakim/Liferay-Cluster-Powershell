f8eaebb659ee1b975c3f2de9f6340179ffab8057
Isaac de Moraes <30375595+Iakim@users.noreply.github.com>
2020-08-19 16:55:37 -0300

    Update elasticsearch.ps1

--- elasticsearch.ps1
+++ elasticsearch.ps1
@@ -8,6 +8,12 @@ $hostname = hostname
 $ip = Get-NetIPAddress -AddressFamily IPv4
 $cluster_name = "liferay-cluster"
 
+# Install Java
+$URL=(Invoke-WebRequest -UseBasicParsing https://www.java.com/en/download/manual.jsp).Content | %{[regex]::matches($_, '(?:<a title="Download Java software for Windows Online" href=")(.*)(?:">)').Groups[1].Value}
+Invoke-WebRequest -UseBasicParsing -OutFile jre8.exe $URL
+Execute-Process .\jre8.exe -Arguments '/s INSTALL_SILENT=1 STATIC=0 AUTO_UPDATE=0 WEB_JAVA=1 WEB_JAVA_SECURITY_LEVEL=H WEB_ANALYTICS=0 EULA=0 REBOOT=0 NOSTARTMENU=0 SPONSORS=0 /L c:\temp\jre8.log' -wait
+echo $?
+
 New-Item -ItemType directory -Path C:\elasticsearch-$version
 Invoke-WebRequest https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$version.msi -OutFile C:\elasticsearch-$version.msi
 Start-Process msiexec.exe -Wait -ArgumentList '/I C:\elasticsearch-$version.msi /qn INSTALLDIR="C:\elasticsearch-$version'
