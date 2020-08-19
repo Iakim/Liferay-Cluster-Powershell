14f70a2049abce4a7110086264b1c200535bee8b
Isaac de Moraes <30375595+Iakim@users.noreply.github.com>
2020-08-19 16:02:57 -0300

    Update elasticsearch.ps1

--- elasticsearch.ps1
+++ elasticsearch.ps1
@@ -1,8 +1,10 @@
 ##############################################################################################
 # NAME:                Script for install elasticsearch on Windows Server 2016
 # AUTHORS:             Isaac de Moraes, Wesley Erick, Kaio Santos, Edyel Mitzi and Wesley Lago
-############################################################################
+##############################################################################################
 
 $version = "6.5.1"
+
+New-Item -ItemType directory -Path C:\elasticsearch-$version
 Invoke-WebRequest https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$version.msi -OutFile C:\elasticsearch-$version.msi
-Start-Process msiexec.exe -Wait -ArgumentList '/I C:\elasticsearch-$version.msi /quiet'
+Start-Process msiexec.exe -Wait -ArgumentList '/I C:\elasticsearch-$version.msi /qn INSTALLDIR="C:\elasticsearch-$version'
