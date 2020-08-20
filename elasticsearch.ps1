8bc1be2e671d49632e5f121fc0f98b72413b58cc
Isaac de Moraes <30375595+Iakim@users.noreply.github.com>
2020-08-20 18:28:39 -0300

    Update elasticsearch.ps1

--- elasticsearch.ps1
+++ elasticsearch.ps1
@@ -17,28 +17,45 @@ echo $?
 [System.Environment]::SetEnvironmentVariable('JAVA_HOME','C:\Program Files\Java\jre1.8.0_261',[System.EnvironmentVariableTarget]::Machine)
 
 New-Item -ItemType directory -Path C:\elasticsearch-$version
-Invoke-WebRequest https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$version.msi -OutFile C:\elasticsearch-$version.msi
-Start-Process msiexec.exe -Wait -ArgumentList '/I C:\elasticsearch-$version.msi /qn INSTALLDIR="C:\elasticsearch-$version'
+Invoke-WebRequest https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$version.zip -OutFile C:\elasticsearch-$version\elasticsearch-$version.zip
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+Unzip C:\elasticsearch-$version\elasticsearch-$version.zip C:\elasticsearch-$version\elasticsearch-$version
+Move-Item C:\elasticsearch-$version\elasticsearch-$version\elasticsearch-$version\* C:\elasticsearch-$version
+Remove-Item –recurse -Path C:\elasticsearch-$version\elasticsearch-$version
+Rename-Item C:\elasticsearch-$version\config\elasticsearch.yml C:\elasticsearch-$version\config\elasticsearch.yml.save
+New-Item  C:\elasticsearch-$version\config\elasticsearch.yml
+
 
 # Configure elasticsearch.yaml
-echo "bootstrap.memory_lock: false" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "cluster.name: $cluster_name" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "network.host: $ip" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "node.data: true" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "node.ingest: false" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "node.master: true" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "node.max_local_storage_nodes: 1" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "node.name: $hostname" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "path.data: C:\elasticsearch-$version\data" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "path.logs: C:\elasticsearch-$version\logs" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "xpack.license.self_generated.type: basic" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "xpack.security.enabled: false" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "action.destructive_requires_name: true" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "http.compression: true" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo 'http.cors.allow-origin: "*"' >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "http.cors.enabled: true" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "thread_pool.search.queue_size: 5000" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
-echo "thread_pool.search.min_queue_size: 1000" >> C:\elasticsearch-$version\conf\elasticsearch.yaml
+echo "bootstrap.memory_lock: false" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "cluster.name: $cluster_name" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "network.host: $ip" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "node.data: true" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "node.ingest: false" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "node.master: true" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "node.max_local_storage_nodes: 1" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "node.name: $hostname" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "path.data: C:\elasticsearch-$version\data" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "path.logs: C:\elasticsearch-$version\logs" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "xpack.license.self_generated.type: basic" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "xpack.security.enabled: false" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "action.destructive_requires_name: true" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "http.compression: true" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo 'http.cors.allow-origin: "*"' >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "http.cors.enabled: true" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "thread_pool.search.queue_size: 5000" >> C:\elasticsearch-$version\config\elasticsearch.yml
+echo "thread_pool.search.min_queue_size: 1000" >> C:\elasticsearch-$version\config\elasticsearch.yml
 
-# Start service
-Get-Service Elasticsearch | Start-Service
+# Install Plugins
+cmd.exe /k "C:\elasticsearch-$version\bin\elasticsearch-plugin.bat install analysis-icu"
+cmd.exe /k "C:\elasticsearch-$version\bin\elasticsearch-plugin.bat install analysis-kuromoji"
+cmd.exe /k "C:\elasticsearch-$version\bin\elasticsearch-plugin.bat install analysis-smartcn"
+cmd.exe /k "C:\elasticsearch-$version\bin\elasticsearch-plugin.bat install analysis-stempel"
+
+# Start
+cmd.exe /k "C:\elasticsearch-$version\bin\elasticsearch.bat"
